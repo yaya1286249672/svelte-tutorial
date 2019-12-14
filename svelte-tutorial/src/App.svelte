fd675ff4769784a834b50f394612ef00091fb308
王娅 <wangya@wangyadeMBP.bbrouter>
2019-12-14 23:37:36 +0800

    feat: 添加组件

--- svelte-tutorial/src/App.svelte
+++ svelte-tutorial/src/App.svelte
@@ -1,12 +1,19 @@
 <script>
 	export let name;
 	import Add from './add.svelte'
+	import Widget from './Widget.svelte'
+	import LinkList from './LinkList.svelte'
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<Add/>
+<Widget>
+	<h1 slot="header">Hello</h1>
+	<p slot="footer">Copyright (c) 2019 Svelte Industries</p>
+</Widget>
+<LinkList/>
 </main>
 
 <style>
--- svelte-tutorial/src/add.svelte
+++ svelte-tutorial/src/add.svelte
@@ -1,29 +1,8 @@
 <script>
 	let a = 1;
-    let b = 2;
-    const list = [];
-  for (let i = 0; i < 50000; i++) {
-    list.push(i);
-  }
-  let data = list;
-    function click(index) {
-    return () => {
-      data[index] = "test";
-    };
-  }
+  let b = 2;  
 </script>
-
 <input type="number" bind:value={a}>
+
 <input type="number" bind:value={b}>
 <p>{a} + {b} = {a + b}</p>
-{#each data as d, i}
-<h1 on:click={click(i)}>
-    <span>
-      <span>
-        <span>
-          <span>{i} {d}!</span>
-        </span>
-      </span>
-    </span>
-  </h1>
-  {/each}
